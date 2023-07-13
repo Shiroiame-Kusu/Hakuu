bf24c15a72fc0ce5bb6bed6d0f1a3e77184e2b4f
Shiroiame-Kusu <kusu@kusu.moe>
2023-07-13 15:54:07 +0800

    Ver 0.4.0-A1: Halfway there.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Serein - 新时代极简服务器面板
+# Serein For JE - 新时代极简服务器面板
 
 <p align="center">
     <img alt="Serein" src="https://socialify.git.ci/Zaitonn/Serein/image?description=1&font=KoHo&logo=https%3A%2F%2Fserein.cc%2Fimg%2FSerein.png&name=1&owner=1&pattern=Circuit%20Board">
     <br>
     <img alt="GitHub Stars" src="https://img.shields.io/github/stars/Zaitonn/Serein?color=blue">
-    <a href="https://github.com/Zaitonn/Serein/releases/latest">
+    <a href="https://github.com/Shiroiame-Kusu/Serein/releases/latest">
         <img src="https://img.shields.io/github/v/release/Zaitonn/Serein?color=blue">
     </a>
-    <a href="https://github.com/Zaitonn/Serein/releases/latest">
+    <a href="https://github.com/Shiroiame-Kusu/Serein/releases/latest">
         <img alt="GitHub all releases" src="https://img.shields.io/github/downloads/Zaitonn/Serein/total?color=blue">
     </a>
-    <a href="https://github.com/Zaitonn/Serein/actions/workflows/Build.yml">
+    <a href="https://github.com/Shiroiame-Kusu/Serein/actions/workflows/Build.yml">
     <img alt="GitHub bulid" src="https://img.shields.io/github/actions/workflow/status/Zaitonn/Serein/Build.yml?branch=main&color=blue">
     </a>
-    <a href="https://github.com/Zaitonn/Serein">
+    <a href="https://github.com/Shiroiame-Kusu/Serein">
         <img alt="GitHub repo file count" src="https://img.shields.io/github/languages/code-size/Zaitonn/Serein">
     </a>
     <a href="https://app.codacy.com/gh/Zaitonn/Serein/">
@@ -23,8 +23,7 @@
 
 - 一个基于`.NET 6`和`NET Framework 4.7.2`的新时代我的世界极简服务器面板
 - **官网：<https://serein.cc/>**
-- **下载最新版：[Releases](https://github.com/Zaitonn/Serein/releases/latest)**
-- Minebbs：[Serein - Minebbs 我的世界中文论坛](https://www.minebbs.com/resources/serein.4169/)
+- **下载最新版：[Releases](https://github.com/Shiroiame-Kusu/Serein/releases/latest)**
 
 ## 建议运行环境
 
@@ -34,32 +33,11 @@ Win7或WinServer2012以上
 
 ## 使用的Package包
 
-详见 <https://github.com/Zaitonn/Serein/network/dependencies>
+详见 <https://github.com/Shiroiame-Kusu/Serein/network/dependencies>
 
 ## 编译
 
 NET SDK >= 6.0
-
-## 发电
-
->如果觉得这个软件对你很有帮助的话可以稍微鼓励一下  
-我会很感谢你的(*￣3￣)╭❤
-
-<details>
-  <summary>发电链接</summary>
-  <img src="https://serein.cc/imgs/afdian.png">
-</details>
-
-| Supporters                                                                        | 发电金额 | 日期       |
-| --------------------------------------------------------------------------------- | -------: | ---------- |
-| [闻王昌龄左迁龙标遥有此寄](https://afdian.net/u/b2407ca0074511ed843c52540025c377) |  ￥ 35.00 | 2023.3.12  |
-| [@SOURsLEMONS](https://github.com/SOURsLEMONS)                                    |  ￥ 10.00 | 2023.2.1   |
-| 滴滴冉滴滴                                                                        |  ￥ 20.00 | 2023.2.1   |
-| [xianyubb](https://www.minebbs.com/members/xianyubb.42760/)                       |   ￥ 5.00 | 2022.11.13 |
-| [c设阿森_Asen](https://q1.qlogo.cn/g?nk=1549033363&b=qq&s=640)                    |  $ 10.00 | 2022.7.25  |
-| [binggggg](https://www.minebbs.com/members/binggggg.12096/)                       |   ￥20.00 | 2022.7.17  |
-| [binggggg](https://www.minebbs.com/members/binggggg.12096/)                       |   ￥20.00 | 2022.7.10  |
-| [binggggg](https://www.minebbs.com/members/binggggg.12096/)                       |   ￥20.00 | 2022.7.9   |
 
 ## 关于
 
@@ -71,6 +49,4 @@ NET SDK >= 6.0
 
 ---
 
-![starchart](https://starchart.cc/Zaitonn/Serein.svg)
-
-![Alt](https://repobeats.axiom.co/api/embed/d29d2773958747edfeed6ee9f280c17f781882cb.svg "Repobeats analytics image")
+![Alt](https://repobeats.axiom.co/api/embed/7a3460be03dc55945411aa4d390d740de9a146fe.svg "Repobeats analytics image")
--- Serein/WPF/Windows/Pages/Server/Download.xaml.cs
+++ Serein/WPF/Windows/Pages/Server/Download.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,42 +19,117 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
+using Wpf.Ui.Dpi;
 
 namespace Serein.Windows.Pages.Server
 {
     /// <summary>
     /// Interaction logic for Download.xaml
     /// </summary>
+    
     public partial class Download : UiPage
     {
+        public static string? API = "https://download.fastmirror.net/api/v3";
+        public static string APIResult {  get; set; }
+        public static string APIStatusCode { get; set; }
+        public static int DownloadableServerNumber {get; set; }
+        public static int DownloadableServerVersion { get; set; }
+        public static string DetailedAPIStatusCode { get; private set; }
+        public static string DetailedAPIResult { get; private set; }
+
         public Download()
         {
-            string API = "https://download.fastmirror.net/api/v3";
             InitializeComponent();
-            //var APIResponse = RequestAPI(API).ToString();
-            //JObject APIDataPrase = JObject.Parse(APIResponse);
-            /*for (int i = 0; i < APIDataPrase.Count; i++)
-            {
-                
-            }*/
-        }
-
-        private void ServerDownload(object sender, RoutedEventArgs e)
-        {
+            LoadAPIInfo();
             
         }
-        private static void Downloader()
+
+        private async void ServerDownload(object sender, RoutedEventArgs e)
+        {
+            ServerDownloadLogTextBox.Clear();
+            var ServerName = ServerDownloadName.SelectedItem.ToString();
+            var ServerVersion = ServerDownloadVersion.SelectedItem.ToString();
+            ServerDownloadLogTextBox.AppendText($"正在下载服务端\n名称：{ServerName}\n版本：{ServerVersion}\n");
+            try
+            {
+                await RequestDetailedAPI(API + ServerDownloadName.SelectedItem + "/" + ServerDownloadVersion.SelectedItem);
+                if (DetailedAPIStatusCode != "200" || DetailedAPIStatusCode != "OK")
+                {
+                    Logger.MsgBox("无法连接至服务器，请检查您的网络连接", "Serein", 0, 48);
+
+                }
+                JObject DetailedAPIPrase = JObject.Parse(DetailedAPIResult);
+                Downloader("https://download.fastmirror.net/download/" + ServerDownloadName.SelectedItem + "/" + ServerDownloadVersion.SelectedItem + "/" + DetailedAPIPrase["data"]["builds"]["core_version"]);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+
+
+        }
+        private static void Downloader(string url)
         {
             
         } 
-        private static void RequestAPI(string url)
+        private async void LoadAPIInfo()
+        {
+            await RequestAPI(API); 
+            try{
+                if (APIStatusCode != "200" || APIStatusCode != "OK")
+                {
+                    Logger.MsgBox("无法连接至服务器，请检查您的网络连接", "Serein", 0, 48);
+
+                }
+
+            }
+            catch(Exception ex){
+                
+            }
+
+            JObject APIDataPrase = JObject.Parse(APIResult);
+            DownloadableServerNumber = APIDataPrase["data"].Count();
+            for(int i = 0; i < DownloadableServerNumber; i++)
+            {
+                ServerDownloadName.Items.Add(APIDataPrase["data"][i]["name"]);
+            }
+            
+        }
+       
+        private static async Task RequestAPI(string url)
         {
             var HttpClient = new HttpClient();
-            var Response =  HttpClient.GetAsync(url);
+            var Response = await HttpClient.GetAsync(url);
             var StatusCode = Response.StatusCode;
             var Header = Response.Headers;
             var APIResponse =  Response.Content.ReadAsStringAsync();
-            return APIResponse;
+            APIStatusCode = StatusCode.ToString();
+            APIResult = APIResponse.Result.ToString();
+           
+        }
+        private static async Task RequestDetailedAPI(string url)
+        {
+            var HttpClient = new HttpClient();
+            var Response = await HttpClient.GetAsync(url);
+            var StatusCode = Response.StatusCode;
+            var Header = Response.Headers;
+            var APIResponse = Response.Content.ReadAsStringAsync();
+            DetailedAPIStatusCode = StatusCode.ToString();
+            DetailedAPIResult = APIResponse.Result.ToString();
+
+        }
+
+        private void ServerDownloadName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var i = ServerDownloadName.SelectedIndex;
+            JObject APIDataPrase = JObject.Parse(APIResult);
+            DownloadableServerVersion = APIDataPrase["data"][i]["mc_versions"].Count();
+            ServerDownloadVersion.Items.Clear();
+            for (int i2 = 0;i2 < DownloadableServerVersion; i2++)
+            {
+                ServerDownloadVersion.Items.Add(APIDataPrase["data"][i]["mc_versions"][i2]);
+            }
         }
     }
 }
