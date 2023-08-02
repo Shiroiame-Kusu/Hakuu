195571ce1a6f79a42657ccb9022744b889a25308
楠ちゃん <kusu@kusu.moe>
2023-08-02 22:58:44 +0800

    Ver 0.7.0-A1

--- Serein/Universal/Global.cs
+++ Serein/Universal/Global.cs
@@ -20,7 +20,7 @@ namespace Serein
         /// <summary>
         /// 版本号
         /// </summary>
-        public const string VERSION = "v0.6.0-B2";
+        public const string VERSION = "v0.7.0-A1";
 
         /// <summary>
         /// 类型
--- Serein/WPF/Windows/Pages/Server/Usage.xaml.cs
+++ Serein/WPF/Windows/Pages/Server/Usage.xaml.cs
@@ -14,6 +14,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Controls;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Esprima.Ast;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Collections;
 
 namespace Serein.Windows.Pages.Server
 {
@@ -22,11 +28,33 @@ namespace Serein.Windows.Pages.Server
     /// </summary>
     public partial class Usage : UiPage
     {
-
+        public int i = 0;
+        public List<double> CPUMonitor = new List<double> { };
+        public ISeries[] LineSeriesCollection_CPU { get; set; }
         public Usage()
         {
             InitializeComponent();
+            CPUMonitor.Add(1);
+            LineSeriesCollection_CPU = new ISeries[]
+                {
+                new LineSeries<double>
+                    {
+                        Values = CPUMonitor,
+                        Fill = null,
+                        Stroke = null
+                }
 
-        }
+
+            };
+            
+            
+       }
     }
+
+
+
+
 }
+    
+
+
